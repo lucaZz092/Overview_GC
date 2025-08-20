4b0763eff8f9f7f5e5c2205870eefd077910fcfe
Lucas Mendonca <lucacampeao2013@gmail.com>
2025-08-20 17:44:31 -0300

    fix: remove componentTagger do vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ['eb26b9d1771c.ngrok-free.app'], // <--- adicione esta linha
-    // Se quiser liberar todos os hosts (mais simples para dev)
+    allowedHosts: ['eb26b9d1771c.ngrok-free.app'], // pode deixar apenas isso ou trocar por 'all'
     // allowedHosts: 'all',
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
